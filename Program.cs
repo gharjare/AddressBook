9c5c5607accd4b928dcbd93b334412ff164ca18b
gharjare <gharjarechetan@gmail.com>
2023-01-07 15:27:14 +0530

    [chetan gharjare]:add wlcm msg

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AddressBook;
+
+namespace Addressbook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Program");
+            Person.CreateContact();
+            Person.editContact();
+            Person.RemoveContact();
+            Person.NewPerson();
+            Person.PrintList();
+            Person.AddMultipleContact();
+            Person.CheckDuplicate();
+            
+        }
+    }
+}
