24c14e69cd49a7610ee9fda05f7da717848110b4
gharjare <gharjarechetan@gmail.com>
2023-01-07 12:36:54 +0530

    [chetan gharjare]:Edit contact of person

--- Person.cs
+++ Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,9 +35,65 @@ namespace AddressBook
             Console.WriteLine("Enter phonenumber");
             conatatct.PhoneNumber = Console.ReadLine();
 
-            Console.WriteLine("Enter email");
-            conatatct.Email = Console.ReadLine();
             person.Add(conatatct);
+
         }
+        public static void editContact()
+        {
+            if (person.Count != 0)
+            {
+                Console.WriteLine("Enter the contct to edit");
+                string Edit = Console.ReadLine();
+
+                foreach (var data in person)
+                {
+                    if (person.Contains(data))
+                    {
+                        Console.WriteLine("for edit contact enter 1.FirstName\n 2.LastNamee\n 3.Address\n 4.City\n 5.State\n 6.Zip\n 7.PhoneNumber\n");
+                        int check = Convert.ToInt32(Console.ReadLine());
+                        switch (check)
+                        {
+                            case 1:
+                                Console.WriteLine("Eneter the First Name");
+                                data.FName = Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the last name");
+                                data.LName = Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter the Address");
+                                data.Addresss = Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter the City");
+                                data.Addresss = Console.ReadLine();
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter the State");
+                                data.Addresss = Console.ReadLine();
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter the Zip");
+                                data.Addresss = Console.ReadLine();
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter the PhonenNmber");
+                                data.Addresss = Console.ReadLine();
+                                break;
+                            default:
+                                Console.WriteLine("Enter the valid option");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Doest choose valid option");
+                    }
+
+                }
+            }
+        }
+            
     }
 }
