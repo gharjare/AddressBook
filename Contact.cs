4de288b0e8cea7d82f6d7fb0fda55873e51fdf9a
gharjare <gharjarechetan@gmail.com>
2023-01-07 11:49:30 +0530

    [chetan gharjare]:create one model class

---
+++ Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Addresss { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
